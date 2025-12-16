0c8b1704cd0911d4492393bcfbf68d92745a1d3a
HyeokJung803 <jh010803@naver.com>
2025-12-16 09:13:23 +0900

    first commit

---
+++ src/test/java/com/example/post_mini_projectback/PostMiniProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.post_mini_projectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMiniProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
